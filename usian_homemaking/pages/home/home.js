9c34084ecd8e8402e2b795da027598ea847d88be
cstp <759168442@qq.com>
2021-12-28 09:38:53 +0800

    feta:实现广场页面的tabs

--- usian_homemaking/pages/home/home.js
+++ usian_homemaking/pages/home/home.js
@@ -1,6 +1,15 @@
 Page({
-    data: {},
+    data: {
+        tabs : ['全部服务','在提供','正在找'],
+        currentTabIndex : 0
+    },
     onLoad: function (options) {
 
+    },
+    handleTabChange : function (event){
+        let index = event.currentTarget.dataset.index
+        this.setData({
+            currentTabIndex : index
+        })
     }
 });
