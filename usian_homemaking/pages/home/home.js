8c0efd4341ad0d5c36daa635f412ea1bb5f6b3ea
cstp <759168442@qq.com>
2021-12-28 11:03:35 +0800

    feta:实现广场页面的分类模块

--- usian_homemaking/pages/home/home.js
+++ usian_homemaking/pages/home/home.js
@@ -1,7 +1,21 @@
 Page({
     data: {
         tabs : ['全部服务','在提供','正在找'],
-        currentTabIndex : 0
+        currentTabIndex : 0,
+        categoryList : [
+            {
+                "id": 1,
+                "name": "保洁"
+            },
+            {
+                "id": 2,
+                "name": "汽修"
+            },
+            {
+                "id": 3,
+                "name": "疏通"
+            }
+        ]
     },
     onLoad: function (options) {
 
@@ -11,5 +25,9 @@ Page({
         this.setData({
             currentTabIndex : index
         })
+    },
+    handleCategoryChange : function(event){
+        let id = event.currentTarget.dataset.id
+        console.log(id)
     }
 });
