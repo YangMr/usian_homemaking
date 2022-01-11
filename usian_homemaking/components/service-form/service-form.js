905f1f2de8680ba99d702e5b79ac225a64e1c2dc
cstp <759168442@qq.com>
2022-01-11 10:59:28 +0800

    feta: 实现发布服务业务逻辑(一)

--- usian_homemaking/components/service-form/service-form.js
+++ usian_homemaking/components/service-form/service-form.js
@@ -1,16 +1,119 @@
+import serviceType from "../../enum/service-type";
+import {getDataSet, getEventParams} from "../../utils/utils";
+import Category from "../../model/category";
+
 Component({
-    properties: {},
+    properties: {
+        //接收的是修改服务传递过来的表单数据
+        form: {
+            type: Object
+        }
+    },
     data: {
-        array : [
+        files : [],
+        typeList: [
             {
-                id : 1,
-                name : '提供服务'
+                id: serviceType.PROVIDE,
+                name: '提供服务'
             },
             {
-                id : 2,
-                name : '正在找'
+                id: serviceType.SEEK,
+                name: '找服务'
             }
-        ]
+        ],
+        //保存的是表单要提交的数据
+        formData: {
+            type: null,
+            title: "",
+            category_id: null,
+            cover_image_id: null,
+            description: "",
+            designated_place: false,
+            begin_date: "",
+            end_date: "",
+            price: ""
+        },
+        categoryList : [],
+        typePickerIndex: null,
+        categoryPickerIndex: null
     },
-    methods: {}
+    lifetimes: {
+        attached: function () {
+            // 在组件实例进入页面节点树时执行
+            this._init()
+        },
+    },
+    methods: {
+        _init: async function () {
+
+            const categoryList = await Category.getCategoryList()
+            const typePickerIndex = this.data.typeList.findIndex(item => item.id === this.data.form.type)
+            const categoryPickerIndex = categoryList.findIndex(item => item.id === this.data.form.category_id)
+            this.setData({
+                categoryList,
+                typePickerIndex: typePickerIndex !== -1 ? typePickerIndex : null,
+                categoryPickerIndex : categoryPickerIndex !== -1 ? categoryPickerIndex : null,
+                formData: {
+                    type: this.data.form.type,
+                    title: this.data.form.title,
+                    category_id: this.data.form.category_id,
+                    cover_image_id: this.data.form.cover_image_id,
+                    designated_place: this.data.form.designated_place,
+                    description: this.data.form.description,
+                    begin_date: this.data.form.begin_date,
+                    end_date: this.data.form.end_date,
+                    price: this.data.form.price
+                }
+            })
+        },
+        handleSubmit : function (){
+            console.log(this.data.formData)
+        },
+        handlePickerChange: function (event) {
+            const typePickerIndex = getEventParams(event, "value")
+            this.setData({
+                typePickerIndex: typePickerIndex,
+                ['formData.type'] : this.data.typeList[typePickerIndex].id
+            })
+        },
+        handleInput : function (event){
+            const value = getEventParams(event,"value")
+            const field = getDataSet(event,'field')
+            this.setData({
+                [`formData.${field}`] : value
+            })
+        },
+        handleCategoryChange : function (event){
+            const categoryPickerIndex = getEventParams(event,"value")
+            this.setData({
+                categoryPickerIndex,
+                ['formData.category_id'] : this.data.categoryList[categoryPickerIndex].id
+            })
+        },
+        handleSwitchChange : function (event){
+            const res = getEventParams(event,"value")
+            this.setData({
+                ['formData.designated_place'] : res
+            })
+        },
+        handleBeginDateChange : function (event){
+            const beginDate = getEventParams(event,'value')
+            this.setData({
+                ['formData.begin_date'] : beginDate
+            })
+        },
+        handleEndDateChange : function (event){
+            const endDate = getEventParams(event,'value')
+            this.setData({
+                ['formData.end_date'] : endDate
+            })
+        },
+        handleSelectImage : function (event){
+            console.log("123")
+            console.log(event)
+        },
+        handleUploadImage : function (event){
+            console.log("456")
+        }
+    }
 });
--- usian_homemaking/pages/publish/publish.js
+++ usian_homemaking/pages/publish/publish.js
@@ -1,5 +1,17 @@
 Page({
-    data: {},
+    data: {
+        formData : {
+            type: null,
+            title: "",
+            category_id: null,
+            cover_image_id: null,
+            description: "",
+            designated_place: false,
+            begin_date: "",
+            end_date: "",
+            price: ""
+        }
+    },
     onLoad: function (options) {
 
     }
